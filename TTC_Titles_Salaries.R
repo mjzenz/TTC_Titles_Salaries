505b7913b51322f3ce7453f45435b89f912dede3
Michael J Zenz <60303194+mjzenz@users.noreply.github.com>
2023-02-10 23:11:24 -0600

    Update TTC_Titles_Salaries.R
    Added years of service quartiles and plot

--- TTC_Titles_Salaries.R
+++ TTC_Titles_Salaries.R
@@ -4,6 +4,10 @@ library(readr)
 
 Title_Salary_Data <- read_csv("Salary_2022-08.csv")
 
+Title_Salary_Data$YearsOfService <- time_length(difftime(Sys.Date(), 
+                                              as.Date(Title_Salary_Data$`Date of Hire`, 
+                                            format = "%d%b%Y %H:%M:%S")), "years") 
+
 
 
 title.salary.dist <- Title_Salary_Data %>%
@@ -14,11 +18,25 @@ title.salary.dist <- Title_Salary_Data %>%
                               median = round(median(`Current Annual Contracted Salary`), 2),
                               third = round(quantile(`Current Annual Contracted Salary`, .75), 2),
                               max = round(max(`Current Annual Contracted Salary`), 2),
-                              n = n_distinct(paste(`First Name`, `Last Name`)))
+                              n = n_distinct(paste(`First Name`, `Last Name`)),
+                              years_min = round(min(YearsOfService), 2),
+                              years_first = round(quantile(YearsOfService, .25), 2),
+                              years_median = round(median(YearsOfService), 2),
+                              years_third = round(quantile(YearsOfService, .75), 2),
+                              years_max = round(max(YearsOfService), 2))
 
-write.csv(title.salary.dist, file = "Title_Salary_Quartiles.csv", row.names = FALSE)
+
+
+write.csv(title.salary.dist, file = "Title_Salary_Years_Quartiles.csv", row.names = FALSE)
 
 
 
-
+Title_Salary_Data %>% filter(`Job Code` %in% c("FN008", "FN007", "FN006")) %>%
+  ggplot(aes(x = YearsOfService, y = `Current Annual Contracted Salary`)) +
+    geom_point(aes(color = Title)) +
+  geom_smooth(method=lm)
+  
+fs_lm <- Title_Salary_Data %>% filter(`Job Code` %in% c("FN008", "FN007", "FN006")) %>%
+        lm(`Current Annual Contracted Salary` ~ YearsOfService + `Job Code`, data = .)
+summary(fs_lm)
                     
